598453248204edd054e850d5478a7634ad0c92f5
Andrew Kozinsky <andkozinskiy@yandex.ru>
2020-05-30 14:14:20 +0500

    I have set some changes to prerare for deploy to Heroku

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+
 
 
 const app = express();
 
+/*app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hello from the server side!'
+    })
+});*/
+
+app.get('/', (req, res) => {
+    res.status(200).send(`<h1>Hello, ${process.env.PORT}</h1>`)
+});
 
 
-
+app.listen(process.env.PORT, () => {
+    console.log('Server started');
+});
