7503bf48988d43e200b82c4838b32d290dc845db
Andrew Kozinsky <andkozinskiy@yandex.ru>
2020-07-11 18:04:47 +0500

    Sign up page made

--- app/src/browserConfig.js
+++ app/src/browserConfig.js
@@ -2,6 +2,7 @@
 // Находится ли сайт в режиме разработки
 const isDevelopment = location.host.startsWith('localhost')
 
+
 /*const serverOrigin = isDevelopment
     ? location.protocol + '//' + location.hostname + ':3000' : ''*/
 const serverOrigin = ''
