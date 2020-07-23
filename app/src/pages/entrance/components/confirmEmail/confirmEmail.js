b69bfc2dd1d230e31a16dc20196291c4b75b7cf1
Andrew Kozinsky <andkozinskiy@yandex.ru>
2020-07-23 15:51:15 +0500

    I fixed a route processing order

--- app/src/pages/entrance/components/confirmEmail/confirmEmail.js
+++ app/src/pages/entrance/components/confirmEmail/confirmEmail.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import {Redirect, useParams} from "react-router-dom";
-const {confirmEmail} = require("./js/resources");
+import React, {useEffect, useState} from 'react'
+import {Redirect, useParams} from "react-router-dom"
+import {confirmEmail} from "./js/resources"
 
 
 function ConfirmEmail() {
@@ -17,7 +17,6 @@ function ConfirmEmail() {
     }, [])
     
     if(needToGoToEnterPage) return <Redirect to='/notes' />
-    
     
         
     return <p>Your email or wasn't registered or is registered. Please try again or use another email.</p>
